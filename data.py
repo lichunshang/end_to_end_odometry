2c1807cb91084543a812fd765c051893c8b92127
Chunshang Li <jack.chunshang.li@gmail.com>
2018-03-27 13:03:42 -0400

    renamed and cleaned up code

--- data.py
+++ data.py
@@ -13,13 +13,12 @@ class StatefulDataGen(object):
     def __init__(self, base_dir, sequences):
         self.truncated_seq_sizes = []
         self.end_of_sequence_indices = []
-        self.curr_batch = 0
-        self.
+        self.curr_batch_idx = 0
 
         total_num_examples = 0
 
         for seq in sequences:
-            seq_data = pykitti.odometry(base_dir, seq, frames=range(0, 20))
+            seq_data = pykitti.odometry(base_dir, seq)
             num_frames = len(seq_data.poses)
 
             # less than timesteps number of frames will be discarded
@@ -28,12 +27,12 @@ class StatefulDataGen(object):
             total_num_examples += num_examples
 
         # less than batch size number of examples will be discarded
-        examples_per_batch = total_num_examples // config.batch_size
+        self.total_batch_count = total_num_examples // config.batch_size
         # +1 adjusts for the extra image in the last time step
-        frames_per_batch = examples_per_batch * (config.timesteps + 1)
+        total_timesteps = self.total_batch_count * (config.timesteps + 1)
 
         # since some examples will be discarded, readjust the truncated_seq_sizes
-        deleted_frames = (total_num_examples - examples_per_batch * config.batch_size) * config.timesteps
+        deleted_frames = (total_num_examples - self.total_batch_count * config.batch_size) * config.timesteps
         for i in range(len(self.truncated_seq_sizes) - 1, -1, -1):
             if self.truncated_seq_sizes[i] > deleted_frames:
                 self.truncated_seq_sizes[i] -= deleted_frames
@@ -44,9 +43,9 @@ class StatefulDataGen(object):
 
         # for storing all training
         self.input_frames = np.zeros(
-            [frames_per_batch, config.batch_size, config.input_channels, config.input_height, config.input_width],
+            [total_timesteps, config.batch_size, config.input_channels, config.input_height, config.input_width],
             dtype=np.uint8)
-        poses_wrt_g = np.zeros([frames_per_batch, config.batch_size, 4, 4], dtype=np.float32)  # ground truth poses
+        poses_wrt_g = np.zeros([total_timesteps, config.batch_size, 4, 4], dtype=np.float32)  # ground truth poses
 
         num_image_loaded = 0
         for i_seq, seq in enumerate(sequences):
@@ -58,8 +57,8 @@ class StatefulDataGen(object):
                 if i_img % 50 == 0:
                     print("Loading sequence %s %.1f%% " % (seq, (i_img / length) * 100))
 
-                i = num_image_loaded % frames_per_batch
-                j = num_image_loaded // frames_per_batch
+                i = num_image_loaded % total_timesteps
+                j = num_image_loaded // total_timesteps
 
                 # swap axis to channels first
                 img = seq_data.get_cam0(i_img)
@@ -73,12 +72,10 @@ class StatefulDataGen(object):
                 poses_wrt_g[i, j] = pose
                 num_image_loaded += 1
 
-                print(i_img)
-
                 # if at end of a sequence
                 if i_img != 0 and i_img != length - 1 and i_img % config.timesteps == 0:
-                    i = num_image_loaded % frames_per_batch
-                    j = num_image_loaded // frames_per_batch
+                    i = num_image_loaded % total_timesteps
+                    j = num_image_loaded // total_timesteps
                     self.input_frames[i, j] = img
                     poses_wrt_g[i, j] = pose
 
@@ -87,15 +84,13 @@ class StatefulDataGen(object):
 
                     num_image_loaded += 1
 
-                    print(i_img)
-
                 gc.collect()  # force garbage collection
 
         # make sure everything is fully loaded
-        assert (num_image_loaded == frames_per_batch * config.batch_size)
+        assert (num_image_loaded == total_timesteps * config.batch_size)
 
         # now convert all the ground truth from 4x4 to xyz + quat, this is after the SE3 layer
-        self.se3_ground_truth = np.zeros([frames_per_batch, config.batch_size, 7], dtype=np.float32)
+        self.se3_ground_truth = np.zeros([total_timesteps, config.batch_size, 7], dtype=np.float32)
         for i in range(0, self.se3_ground_truth.shape[0]):
             for j in range(0, self.se3_ground_truth.shape[1]):
                 translation = transformations.translation_from_matrix(poses_wrt_g[i, j])
@@ -103,7 +98,7 @@ class StatefulDataGen(object):
                 self.se3_ground_truth[i, j] = np.concatenate([translation, quat])
 
         # extract the relative transformation between frames after the fully connected layer
-        self.fc_ground_truth = np.zeros([frames_per_batch, config.batch_size, 6], dtype=np.float32)
+        self.fc_ground_truth = np.zeros([total_timesteps, config.batch_size, 6], dtype=np.float32)
         # going through rows, then columns
         for i in range(0, self.fc_ground_truth.shape[0]):
             for j in range(0, self.fc_ground_truth.shape[1]):
@@ -114,15 +109,9 @@ class StatefulDataGen(object):
                 else:
                     m = np.dot(np.linalg.inv(poses_wrt_g[i - 1, j]), poses_wrt_g[i, j])  # double check
 
-                print(m)
-
                 translation = transformations.translation_from_matrix(m)
                 ypr = transformations.euler_from_matrix(m, axes="rzyx")
                 self.fc_ground_truth[i, j] = np.concatenate([translation, ypr])  # double check
-
-        # print(poses)
-        # print(self.se3_ground_truth)
-        print(self.fc_ground_truth)
 
     def next_batch(self):
         pass
