741414db0b569a4f1697c7ab9a46d5810e51e7e2
Chunshang Li <jack.chunshang.li@gmail.com>
2018-04-03 16:54:58 -0400

    combined cost function and set alpha schedule

--- train_seq.py
+++ train_seq.py
@@ -28,16 +28,15 @@ with tf.device("/gpu:0"):
     with tf.variable_scope("Losses"):
         se3_losses = losses.se3_losses(se3_outputs, se3_labels, cfg.k)
         fc_losses = losses.fc_losses(fc_outputs, fc_labels)
+        alpha = tf.placeholder(tf.float32, name="alpha", shape=[])  # between 0 and 1, larger favors fc loss
+        total_losses = (1 - alpha) * se3_losses + alpha * fc_losses
 
 tools.printf("Building optimizer...")
 with tf.variable_scope("Optimizer"):
     # dynamic learning rates
-    se3_lr = tf.placeholder(tf.float32, name="se3_lr", shape=[])
-    fc_lr = tf.placeholder(tf.float32, name="fc_lr", shape=[])
+    lr = tf.placeholder(tf.float32, name="se3_lr", shape=[])
     with tf.device("/gpu:0"):
-        se3_trainer = tf.train.AdamOptimizer(learning_rate=se3_lr).minimize(se3_losses)
-    with tf.device("/gpu:0"):
-        fc_trainer = tf.train.AdamOptimizer(learning_rate=fc_lr).minimize(fc_losses)
+        trainer = tf.train.AdamOptimizer(learning_rate=lr).minimize(total_losses)
 
 
 # for evaluating validation loss
@@ -80,7 +79,14 @@ cnn_init_tf_saver = tf.train.Saver(cnn_variables)
 cnn_init_model_file = "/home/lichunshang/Dev/end_to_end_visual_odometry/results/" \
                       "train_pair_20180402-12-21-24_seq_00_to_05_randomized_dropout(0.9, 0.8, 0.7)/" \
                       "model_best_val_checkpoint"
-
+# =================== TRAINING PARAMS ========================
+lr_set = 0.001
+start_epoch = 0
+alpha_schedule = {0: 0.99,  # epoch: alpha
+                  10: 0.9,
+                  20: 0.5,
+                  30: 0.1,
+                  40: 0.025}
 # =================== TRAINING ========================
 with tf.Session() as sess:
     if cnn_init_model_file:
@@ -99,26 +105,28 @@ with tf.Session() as sess:
     writer.add_graph(tf.get_default_graph())
 
     total_batches = train_data_gen.total_batches()
-    se3_losses_history = np.zeros([cfg.num_epochs, total_batches])
-    fc_losses_history = np.zeros([cfg.num_epochs, total_batches])
-    se3_val_losses_history = np.zeros([cfg.num_epochs, val_data_gen.total_batches()])
-    fc_lr_set = 0.001
-    se3_lr_set = 0.001
+    total_losses_history = np.zeros([cfg.num_epochs, total_batches])
+    val_losses_history = np.zeros([cfg.num_epochs, val_data_gen.total_batches()])
     best_val_loss = 9999999999
+    alpha_set = -1
 
     tools.printf("Start training loop...")
-    for i_epoch in range(cfg.num_epochs):
+    tools.printf("lr: %f" % lr)
+    tools.printf("start_epoch: %f" % start_epoch)
+    tools.printf("alpha_schedule: %s" % alpha_schedule)
+    for i_epoch in range(start_epoch, cfg.num_epochs):
         tools.printf("Training Epoch: %d ..." % i_epoch)
 
-        train_data_gen.next_epoch()
         curr_lstm_states = np.zeros([2, cfg.lstm_layers, cfg.batch_size, cfg.lstm_size])
-
         start_time = time.time()
-        _se3_losses = 0
-        _fc_losses = 0
+        _total_losses = 0
+        epoch_total_losses_history = []
 
-        epoch_se3_losses_history = []
-        epoch_fc_losses_history = []
+        if i_epoch in alpha_schedule.keys():
+            alpha_set = alpha_schedule[i_epoch]
+            tools.printf("alpha set to %f" % alpha_set)
+
+        train_data_gen.next_epoch()
 
         while train_data_gen.has_next_batch():
             init_poses, reset_state, batch_data, \
@@ -126,46 +134,34 @@ with tf.Session() as sess:
 
             curr_lstm_states = data.reset_select_lstm_state(curr_lstm_states, reset_state)
 
-            _fc_outputs, _fc_losses, _fc_trainer = sess.run(
-                [fc_outputs, fc_losses, fc_trainer],
-                feed_dict={
-                    inputs: batch_data,
-                    fc_labels: fc_ground_truth,
-                    lstm_initial_state: curr_lstm_states,
-                    fc_lr: fc_lr_set,
-                    is_training: True
-                }
-            )
-
-            _se3_outputs, _se3_losses, _se3_trainer, _curr_lstm_states = sess.run(
-                [se3_outputs, se3_losses, se3_trainer, lstm_states, ],
+            _total_losses, trainer, _curr_lstm_states = sess.run(
+                [total_losses, trainer, lstm_states, ],
                 feed_dict={
                     inputs: batch_data,
                     se3_labels: se3_ground_truth,
+                    fc_labels: fc_ground_truth,
                     lstm_initial_state: curr_lstm_states,
                     initial_poses: init_poses,
-                    se3_lr: se3_lr_set,
+                    lr: lr_set,
+                    alpha: alpha_set,
                     is_training: True
                 }
             )
 
-            epoch_se3_losses_history.append(_se3_losses)
-            epoch_fc_losses_history.append(_fc_losses)
+            epoch_total_losses_history.append(_total_losses)
 
             curr_lstm_states = _curr_lstm_states
 
             # print stats
-            tools.printf("batch %d/%d: se3_loss: %.3f, fc_loss: %.3f" % (
-                train_data_gen.curr_batch(), train_data_gen.total_batches(), _se3_losses, _fc_losses))
+            tools.printf("batch %d/%d: total_loss: %.3f" % (
+                train_data_gen.curr_batch(), train_data_gen.total_batches(), _total_losses))
 
-        ave_se3_loss = sum(epoch_se3_losses_history) / total_batches
-        ave_fc_loss = sum(epoch_fc_losses_history) / total_batches
+        ave_total_loss = sum(epoch_total_losses_history) / total_batches
 
-        se3_losses_history[i_epoch, :] = epoch_se3_losses_history
-        fc_losses_history[i_epoch, :] = epoch_fc_losses_history
+        total_losses_history[i_epoch, :] = epoch_total_losses_history
 
-        epoch_se3_val_losses, ave_val_loss = calc_val_loss(sess)
-        se3_val_losses_history[i_epoch, :] = epoch_se3_val_losses
+        epoch_val_losses, ave_val_loss = calc_val_loss(sess)
+        val_losses_history[i_epoch, :] = epoch_val_losses
 
         if ave_val_loss < best_val_loss:
             best_val_loss = ave_val_loss
@@ -175,12 +171,11 @@ with tf.Session() as sess:
             tf_saver.save(sess, os.path.join(results_dir_path, "model_epoch_checkpoint"))
             tools.printf("Checkpoint saved")
 
-        tools.printf("Epoch %d, ave_se3_loss: %.3f, ave_fc_loss: %.3f, ave_val_loss: %f, time: %.2f" %
-                     (i_epoch, ave_se3_loss, ave_fc_loss, ave_val_loss, time.time() - start_time))
+        tools.printf("Epoch %d, ave_total_loss: %.3f, ave_val_loss(se3): %f, time: %.2f" %
+                     (i_epoch, ave_total_loss, ave_val_loss, time.time() - start_time))
         tools.printf()
 
-    np.save(os.path.join(results_dir_path, "se3_losses_history"), se3_losses_history)
-    np.save(os.path.join(results_dir_path, "fc_losses_history"), fc_losses_history)
-    np.save(os.path.join(results_dir_path, "se3_val_losses_history"), se3_val_losses_history)
+    np.save(os.path.join(results_dir_path, "total_losses_history"), total_losses_history)
+    np.save(os.path.join(results_dir_path, "val_losses_history_se3"), val_losses_history)
     tf_saver.save(sess, os.path.join(results_dir_path, "model_epoch_checkpoint"))
     tools.printf("Saved results to %s" % results_dir_path)
