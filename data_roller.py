c8b1bd7da37b9a09cd0516ddcf802bf1cdc37a11
Chunshang Li <jack.chunshang.li@gmail.com>
2018-04-16 17:34:13 -0400

    added lidar data

--- data_roller.py
+++ data_roller.py
@@ -1,11 +1,46 @@
 import pykitti
 import numpy as np
-import config
-import gc
 import transformations
 import tools
 import random
-import math as m
+import os
+import pickle
+import gc
+
+
+class LidarDataLoader(object):
+    def __init__(self, config, base_dir, seq, frames):
+        pickles_dir = os.path.join(base_dir, "sequences", "lidar_picks")
+        seq_data = pykitti.odometry(base_dir, seq)
+        num_frames = len(seq_data.poses)
+        self.data = np.zeros([num_frames, 2, config.input_height, config.input_width], dtype=np.float16)
+
+        with (open(pickles_dir + seq + "_range.pik", "rb")) as opfile:
+            i = 0
+            while True:
+                try:
+                    cur_image = pickle.load(opfile)
+                    self.data[i, :, :, 0] = cur_image
+                except EOFError:
+                    break
+                i += 1
+            assert (i == num_frames)
+
+        with (open(pickles_dir + seq + "_intensity.pik", "rb")) as opfile:
+            i = 0
+            while True:
+                try:
+                    cur_image = pickle.load(opfile)
+                    self.data[i, :, :, 0] = cur_image
+                except EOFError:
+                    break
+                i += 1
+            assert (i == num_frames)
+
+        self.data = self.data[frames]
+
+    def get(self, idx):
+        return self.data[idx]
 
 
 class StatefulRollerDataGen(object):
@@ -13,6 +48,7 @@ class StatefulRollerDataGen(object):
     # This version of the data generator slides along n frames at a time
     def __init__(self, config, base_dir, sequences, frames=None):
         self.cfg = config
+        self.data_type = "cam"
         self.sequences = sequences
         self.curr_batch_idx = np.zeros([self.cfg.batch_size], dtype=np.int64)
         self.curr_epoch_sequence = []
@@ -21,6 +57,15 @@ class StatefulRollerDataGen(object):
 
         if (self.cfg.bidir_aug == True) and (self.cfg.batch_size % 2 != 0):
             raise ValueError("Batch size must be even")
+
+        if hasattr(self.cfg.data_src, "data_src"):
+            self.data_type = self.cfg.data_type
+        if self.data_type != "cam" and self.data_type != "lidar":
+            raise Exception("lidar or camera for data type!")
+
+        frames_data_type = np.uint8
+        if self.data_type == "lidar":
+            frames_data_type = np.float16
 
         if not frames:
             frames = [None] * len(sequences)
@@ -36,9 +81,12 @@ class StatefulRollerDataGen(object):
 
         for i_seq, seq in enumerate(sequences):
             seq_data = pykitti.odometry(base_dir, seq, frames=frames[i_seq])
+            lidar_data = LidarDataLoader(self.cfg, base_dir, seq, frames=frames[i_seq])
             num_frames = len(seq_data.poses)
 
-            self.input_frames[seq] = np.zeros([num_frames, self.cfg.input_channels, self.cfg.input_height, self.cfg.input_width], dtype=np.uint8)
+            self.input_frames[seq] = np.zeros(
+                [num_frames, self.cfg.input_channels, self.cfg.input_height, self.cfg.input_width],
+                dtype=frames_data_type)
             self.poses[seq] = seq_data.poses
             self.se3_ground_truth[seq] = np.zeros([num_frames, 7], dtype=np.float32)
             self.fc_ground_truth[seq] = np.zeros([num_frames - 1, 6], dtype=np.float32)
@@ -48,13 +96,16 @@ class StatefulRollerDataGen(object):
                     tools.printf("Loading sequence %s %.1f%% " % (seq, (i_img / num_frames) * 100))
 
                 # swap axis to channels first
-                img = seq_data.get_cam2(i_img)
-                img = img.resize((self.cfg.input_width, self.cfg.input_height))
-                img = np.array(img)
-                if img.shape[2] == 3:  # convert to bgr if colored image
-                    img = img[..., [2, 1, 0]]
-                img = np.reshape(img, [img.shape[0], img.shape[1], self.cfg.input_channels])
-                img = np.moveaxis(np.array(img), 2, 0)
+                if self.data_type == "lidar":
+                    img = lidar_data.get(i_img)
+                else:
+                    img = seq_data.get_cam2(i_img)
+                    img = img.resize((self.cfg.input_width, self.cfg.input_height))
+                    img = np.array(img)
+                    if img.shape[2] == 3:  # convert to bgr if colored image
+                        img = img[..., [2, 1, 0]]
+                    img = np.reshape(img, [img.shape[0], img.shape[1], self.cfg.input_channels])
+                    img = np.moveaxis(np.array(img), 2, 0)
 
                 self.input_frames[seq][i_img, :] = img
 
@@ -72,7 +123,8 @@ class StatefulRollerDataGen(object):
                     self.fc_ground_truth[seq][i_img] = np.concatenate([translation, ypr])  # double check
 
             # How many examples the sequence contains, were it to be processed using a batch size of 1
-            sequence_examples = np.floor((num_frames - self.cfg.timesteps + 1)/self.cfg.sequence_stride).astype(np.int32)
+            sequence_examples = np.floor((num_frames - self.cfg.timesteps + 1) / self.cfg.sequence_stride).astype(
+                np.int32)
             # how many batches in the sequence, cutting off any extra
             if self.cfg.bidir_aug:
                 self.batch_sizes[seq] = np.floor(2 * sequence_examples / self.cfg.batch_size)
@@ -85,13 +137,15 @@ class StatefulRollerDataGen(object):
         tools.printf("All data loaded, batch_size=%d, timesteps=%d, num_batches=%d" % (
             self.cfg.batch_size, self.cfg.timesteps, self.batch_cnt))
 
+        gc.collect()
         self.next_epoch()
 
     def next_batch(self):
         n = self.cfg.timesteps + 1  # number of frames in an example
         # prepare a batch from huge matrix of training data
         reset_state = np.zeros([self.cfg.batch_size], dtype=np.uint8)
-        batch = np.zeros([n, self.cfg.batch_size, self.cfg.input_channels, self.cfg.input_height, self.cfg.input_width], dtype=np.float32)
+        batch = np.zeros([n, self.cfg.batch_size, self.cfg.input_channels, self.cfg.input_height, self.cfg.input_width],
+                         dtype=np.float32)
         se3_ground_truth = np.zeros([n, self.cfg.batch_size, 7], dtype=np.float32)
         fc_ground_truth = np.zeros([self.cfg.timesteps, self.cfg.batch_size, 6], dtype=np.float32)
 
@@ -107,17 +161,17 @@ class StatefulRollerDataGen(object):
             idx = self.curr_batch_idx[i_b]
             if (i_b < self.cfg.batch_size / 2) or not self.cfg.bidir_aug:
                 # data going forwards
-                batch[:,i_b,:,:,:] = self.input_frames[cur_seq][idx:idx + n, :, :, :]
+                batch[:, i_b, :, :, :] = self.input_frames[cur_seq][idx:idx + n, :, :, :]
                 se3_ground_truth[:, i_b, :] = self.se3_ground_truth[cur_seq][idx:idx + n, :]
                 fc_ground_truth[:, i_b, :] = self.fc_ground_truth[cur_seq][idx:idx + n - 1, :]
                 self.curr_batch_idx[i_b] += self.cfg.sequence_stride
             else:
-                #data going backwards
-                batch[:,i_b,:,:] = self.input_frames[cur_seq][idx - n:idx, :, :, :]
+                # data going backwards
+                batch[:, i_b, :, :] = self.input_frames[cur_seq][idx - n:idx, :, :, :]
                 se3_ground_truth[:, i_b, :] = self.se3_ground_truth[cur_seq][idx - n:idx, :]
-                fc_ground_truth[:, i_b, :] = -self.fc_ground_truth[cur_seq][idx - n:idx-1, :]
-                #flip along time axis
-                batch[:,i_b,:,:] = np.flip(batch[:,i_b,:,:], axis=0)
+                fc_ground_truth[:, i_b, :] = -self.fc_ground_truth[cur_seq][idx - n:idx - 1, :]
+                # flip along time axis
+                batch[:, i_b, :, :] = np.flip(batch[:, i_b, :, :], axis=0)
                 se3_ground_truth[:, i_b, :] = np.flip(se3_ground_truth[:, i_b, :], axis=0)
                 fc_ground_truth[:, i_b, :] = np.flip(fc_ground_truth[:, i_b, :], axis=0)
 
@@ -140,12 +194,14 @@ class StatefulRollerDataGen(object):
         for i_b in range(len(self.curr_batch_idx)):
             # first half of batches are going forward in time
             if (i_b < halfway) or not self.cfg.bidir_aug:
-                self.curr_batch_idx[i_b] = self.batch_sizes[self.sequences[self.curr_batch_sequences[i_b]]] * i_b * self.cfg.sequence_stride
+                self.curr_batch_idx[i_b] = self.batch_sizes[self.sequences[
+                    self.curr_batch_sequences[i_b]]] * i_b * self.cfg.sequence_stride
             # second half are going back in time
             else:
                 reverse_start = self.batch_sizes[self.sequences[self.curr_batch_sequences[i_b]]] * (
-                            self.cfg.batch_size / 2) * self.cfg.sequence_stride
-                self.curr_batch_idx[i_b] = reverse_start - self.batch_sizes[self.sequences[self.curr_batch_sequences[i_b]]] * (i_b - halfway) * self.cfg.sequence_stride
+                        self.cfg.batch_size / 2) * self.cfg.sequence_stride
+                self.curr_batch_idx[i_b] = reverse_start - self.batch_sizes[
+                    self.sequences[self.curr_batch_sequences[i_b]]] * (i_b - halfway) * self.cfg.sequence_stride
 
     def next_epoch(self):
         # Randomize sequence order
