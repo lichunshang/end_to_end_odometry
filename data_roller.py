505dcacea197bbea8f510c332238896c803b6de4
Ben <benjamin.skikos@gmail.com>
2018-04-16 17:30:29 -0400

    add ground truth mirroring and more correct reversal

--- data_roller.py
+++ data_roller.py
@@ -27,8 +27,17 @@ class StatefulRollerDataGen(object):
 
         self.input_frames = {}
         self.poses = {}
+
+        # Mirror in y-z plane
+        self.H = np.identity(3, dtype=np.float32)
+        self.H[0][0] = -1.0
+
         self.se3_ground_truth = {}
+        self.se3_mirror_ground_truth = {}
         self.fc_ground_truth = {}
+        self.fc_reverse_ground_truth = {}
+        self.fc_mirror_ground_truth = {}
+        self.fc_reverse_mirror_ground_truth = {}
         self.batch_sizes = {}
 
         self.batch_cnt = 0
@@ -41,17 +50,24 @@ class StatefulRollerDataGen(object):
             self.input_frames[seq] = np.zeros([num_frames, self.cfg.input_channels, self.cfg.input_height, self.cfg.input_width], dtype=np.uint8)
             self.poses[seq] = seq_data.poses
             self.se3_ground_truth[seq] = np.zeros([num_frames, 7], dtype=np.float32)
+            self.se3_mirror_ground_truth[seq] = np.zeros([num_frames, 7], dtype=np.float32)
             self.fc_ground_truth[seq] = np.zeros([num_frames - 1, 6], dtype=np.float32)
+            self.fc_reverse_ground_truth[seq] = np.zeros([num_frames - 1, 6], dtype=np.float32)
+            self.fc_mirror_ground_truth[seq] = np.zeros([num_frames - 1, 6], dtype=np.float32)
+            self.fc_reverse_mirror_ground_truth[seq] = np.zeros([num_frames - 1, 6], dtype=np.float32)
 
             for i_img in range(num_frames):
                 if i_img % 100 == 0:
                     tools.printf("Loading sequence %s %.1f%% " % (seq, (i_img / num_frames) * 100))
 
                 # swap axis to channels first
-                img = seq_data.get_cam2(i_img)
+                if self.cfg.input_channels == 1:
+                    img = seq_data.get_cam0(i_img)
+                elif self.cfg.input_channels == 3:
+                    img = seq_data.get_cam2(i_img)
                 img = img.resize((self.cfg.input_width, self.cfg.input_height))
                 img = np.array(img)
-                if img.shape[2] == 3:  # convert to bgr if colored image
+                if self.cfg.input_channels == 3:
                     img = img[..., [2, 1, 0]]
                 img = np.reshape(img, [img.shape[0], img.shape[1], self.cfg.input_channels])
                 img = np.moveaxis(np.array(img), 2, 0)
@@ -61,15 +77,40 @@ class StatefulRollerDataGen(object):
                 # now convert all the ground truth from 4x4 to xyz + quat, this is after the SE3 layer
                 translation = transformations.translation_from_matrix(self.poses[seq][i_img])
                 quat = transformations.quaternion_from_matrix(self.poses[seq][i_img])
+
+                mirror_pose = np.identity(4, dtype=np.float32)
+                mirror_pose[0:3, 0:3] = np.dot(self.H, np.dot(self.poses[seq][i_img][0:3, 0:3], self.H))
+                mirror_pose[0:3, 3] = np.dot(self.H, translation)
+
+                mirror_quat = transformations.quaternion_from_matrix(mirror_pose[0:3, 0:3])
                 self.se3_ground_truth[seq][i_img] = np.concatenate([translation, quat])
+                self.se3_mirror_ground_truth[seq][i_img] = np.concatenate([mirror_pose[0:3, 3], mirror_quat])
 
                 # relative transformation labels
-                if (i_img + 1 < num_frames):
-                    m = np.dot(np.linalg.inv(self.poses[seq][i_img]), self.poses[seq][i_img + 1])  # double check
-                    translation = transformations.translation_from_matrix(m)
-                    ypr = transformations.euler_from_matrix(m, axes="rzyx")
-                    assert (np.all(np.abs(ypr) < np.pi))
-                    self.fc_ground_truth[seq][i_img] = np.concatenate([translation, ypr])  # double check
+                if i_img + 1 < num_frames:
+                    mirror_pose_next = np.identity(4, dtype=np.float32)
+                    mirror_pose_next[0:3, 0:3] = np.dot(self.H, np.dot(self.poses[seq][i_img + 1][0:3, 0:3], self.H))
+                    trans_next = transformations.translation_from_matrix(self.poses[seq][i_img + 1])
+                    mirror_pose_next[0:3, 3] = np.dot(self.H, trans_next)
+
+                    m_forward = np.dot(np.linalg.inv(self.poses[seq][i_img]), self.poses[seq][i_img + 1])
+                    m_forward_mirror = np.dot(np.linalg.inv(mirror_pose), mirror_pose_next)
+                    m_reverse = np.dot(np.linalg.inv(self.poses[seq][i_img + 1]), self.poses[seq][i_img])
+                    m_reverse_mirror = np.dot(np.linalg.inv(mirror_pose_next), mirror_pose)
+                    
+                    trans_forward = transformations.translation_from_matrix(m_forward)
+                    ypr_forward = transformations.euler_from_matrix(m_forward, axes="rzyx")
+                    trans_forward_mirror = transformations.translation_from_matrix(m_forward_mirror)
+                    ypr_forward_mirror = transformations.euler_from_matrix(m_forward_mirror, axes="rzyx")
+                    trans_reverse = transformations.translation_from_matrix(m_reverse)
+                    ypr_reverse = transformations.euler_from_matrix(m_reverse, axes="rzyx")
+                    trans_reverse_mirror = transformations.translation_from_matrix(m_reverse_mirror)
+                    ypr_reverse_mirror = transformations.euler_from_matrix(m_reverse_mirror, axes="rzyx")
+
+                    self.fc_ground_truth[seq][i_img] = np.concatenate([trans_forward, ypr_forward])
+                    self.fc_mirror_ground_truth[seq][i_img] = np.concatenate([trans_forward_mirror, ypr_forward_mirror])
+                    self.fc_reverse_ground_truth[seq][i_img] = np.concatenate([trans_reverse, ypr_reverse])
+                    self.fc_reverse_mirror_ground_truth[seq][i_img] = np.concatenate([trans_reverse_mirror, ypr_reverse_mirror])
 
             # How many examples the sequence contains, were it to be processed using a batch size of 1
             sequence_examples = np.floor((num_frames - self.cfg.timesteps + 1)/self.cfg.sequence_stride).astype(np.int32)
@@ -115,7 +156,7 @@ class StatefulRollerDataGen(object):
                 #data going backwards
                 batch[:,i_b,:,:] = self.input_frames[cur_seq][idx - n:idx, :, :, :]
                 se3_ground_truth[:, i_b, :] = self.se3_ground_truth[cur_seq][idx - n:idx, :]
-                fc_ground_truth[:, i_b, :] = -self.fc_ground_truth[cur_seq][idx - n:idx-1, :]
+                fc_ground_truth[:, i_b, :] = self.fc_reverse_ground_truth[cur_seq][idx - n:idx-1, :]
                 #flip along time axis
                 batch[:,i_b,:,:] = np.flip(batch[:,i_b,:,:], axis=0)
                 se3_ground_truth[:, i_b, :] = np.flip(se3_ground_truth[:, i_b, :], axis=0)
