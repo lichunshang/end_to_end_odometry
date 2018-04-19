fb4a03ae6ab4c645bd39a431d00dd0b07c3e0ee6
Chunshang Li <jack.chunshang.li@gmail.com>
2018-04-19 11:42:30 -0400

    changes to plots

--- scraps/plot_losses.py
+++ scraps/plot_losses.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tools
 
-data_dir = "/home/cs4li/Dev/end_to_end_visual_odometry/results/trajectory_results/"
+data_dir = "/home/cs4li/Dev/end_to_end_visual_odometry/results/trajectory_results_1/"
 
 losses_to_overlay = [
-    (data_dir + "run_results-tag-se3_losses.csv", {"linewidth": 1.0, "color": "r"}, "Training Loss"),
-    (data_dir + "run_results-tag-training_loss_val.csv", {"linewidth": 1.0, "color": "b"}, "Validation Loss")
+    (data_dir + "run_results_trajectory_results_1-tag-se3_losses.csv", {"linewidth": 1.0, "color": "r"}, "Training Loss"),
+    (data_dir + "run_results_trajectory_results_1-tag-se3_losses_val.csv", {"linewidth": 1.0, "color": "b"}, "Validation Loss")
 ]
 
 plt.figure(1)
@@ -15,21 +15,21 @@ loss_to_draw = losses_to_overlay[0]
 data = np.loadtxt(loss_to_draw[0], skiprows=1, delimiter=",")
 x = data[:, 1] / 797
 y = data[:, 2]
-y = tools.smooth(y, window_len=1500, window="hanning")
+y = tools.smooth(y, window_len=2000, window="hanning")
 plt.plot(x, y[0: len(x)], **loss_to_draw[1], label=loss_to_draw[2])
 
 loss_to_draw = losses_to_overlay[1]
 data = np.loadtxt(loss_to_draw[0], skiprows=1, delimiter=",")
 x = data[:, 1] / 24
 y = data[:, 2]
-y = tools.smooth(y, window_len=50, window="hanning")
+y = tools.smooth(y, window_len=100, window="hanning")
 plt.plot(x, y[0: len(x)], **loss_to_draw[1], label=loss_to_draw[2])
 
 plt.xlabel("epochs")
-plt.ylabel("losses")
+plt.ylabel("SE(3) losses")
 plt.ylim(0, 0.1)
 plt.xlim(0, 151)
-plt.title("Training and Validation Losses")
+plt.title("SE(3) Training and Validation Losses")
 plt.legend()
 plt.savefig(data_dir + "fig_losses.png")
 plt.show()
--- scraps/plot_trajectory.py
+++ scraps/plot_trajectory.py
@@ -8,7 +8,7 @@ sequences = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10"]
 for i, sequence in enumerate(sequences):
 
     trajectories_to_overlay = [
-        (data_dir + "trajectory_%s.npy" % sequence, {"linewidth": 1.0, "color": "r"}, "LiDAR Odometry"),
+        (data_dir + "trajectory10_%s.npy" % sequence, {"linewidth": 1.0, "color": "r"}, "LiDAR Odometry"),
         (data_dir + "ground_truth_%s.npy" % sequence, {"linewidth": 1.0, "color": "b"}, "Ground Truth")
     ]
 
@@ -24,7 +24,15 @@ for i, sequence in enumerate(sequences):
     plt.axis("equal")
     plt.xlabel("x [m]")
     plt.ylabel("z [m]")
-    plt.title("KITTI Sequence %s Trajectory" % sequence)
+
+    if sequence in ["00", "01", "02", "08", "09"]:
+        set_type = "Training Set"
+    elif sequence in ["07"]:
+        set_type = "Validation Set"
+    else:
+        set_type = "Test Set"
+
+    plt.title("KITTI Sequence %s Trajectory (%s)" % (sequence, set_type))
     plt.legend()
     plt.savefig(data_dir + "fig_%s.png" % sequence)
     # plt.show()
