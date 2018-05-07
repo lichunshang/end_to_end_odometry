671bdddb7dfa654225132e5bc81feef54b295421
Chunshang Li <jack.chunshang.li@gmail.com>
2018-05-07 15:20:22 -0400

    added code to ensure dir exist

--- preprocess_lidar.py
+++ preprocess_lidar.py
@@ -5,6 +5,7 @@ import numpy as np
 from scipy.stats import binned_statistic
 import pickle
 import config
+import tools
 
 base_dir = config.dataset_path
 output_dir = config.lidar_pickles_path
@@ -67,9 +68,9 @@ for seq in sequences:
     for i in range(0, len(data.poses)):
         # need to flip them horizontally because the bins are from -pi to pi, we want the
         # image to be from pi (left most) to -pi (right most)
-        pickle.dump(np.fliplr(images[i, 0, :, :].astype(np.float16)), range_out)
-        pickle.dump(np.fliplr((images[i, 1, :, :] * 255.0).astype(np.uint8)), int_out)
-        pickle.dump(np.fliplr((images[i, 2, :, :]).astype(np.bool)), mask_out)
+        pickle.dump(np.fliplr(images[i, 0, :, :].astype(np.float16)), tools.ensure_file_dir_exists(range_out))
+        pickle.dump(np.fliplr((images[i, 1, :, :] * 255.0).astype(np.uint8)), tools.ensure_file_dir_exists(int_out))
+        pickle.dump(np.fliplr((images[i, 2, :, :]).astype(np.bool)), tools.ensure_file_dir_exists(mask_out))
         if i % 100 == 0:
             print("Saving sequence %s %.1f%% " % (seq, (i / len(data.poses)) * 100))
 
