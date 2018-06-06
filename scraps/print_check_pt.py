d1d9a5fa9de05c2375a6787c74f1ffe363c177a6
Ben <benjamin.skikos@gmail.com>
2018-06-06 13:36:45 -0400

    path change

--- scraps/print_check_pt.py
+++ scraps/print_check_pt.py
@@ -1,4 +1,4 @@
 from tensorflow.python.tools import inspect_checkpoint as chkp
 
-chkp.print_tensors_in_checkpoint_file("/home/cs4li/Dev/end_to_end_visual_odometry/results/train_seq_20180411-19-34-59/best_val/model_best_val_checkpoint-7", tensor_name='',
+chkp.print_tensors_in_checkpoint_file("/media/bapskiko/Trailing 24GB/model_epoch_checkpoint-140", tensor_name='',
     all_tensors=False, all_tensor_names=True)
