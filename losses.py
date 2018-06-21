fix normalization before matrix inverse

losses.py
@@ -75,11 +75,12 @@ def fc_losses(outputs, output_covar, labels_u, k):
         # dense covariance
         Q = output_covar
 
-        # determinant of a diagonal matrix is product of it diagonal
         log_det_Q = tf.log(tf.matrix_determinant(Q) + 1e-5)
 
-        # inverse of a diagonal matrix is elemental inverse
-        inv_Q = tf.matrix_inverse(Q + 1e-5)
+        # Need to normalize
+        norm1 = tf.tile(tf.expand_dims(tf.diag(1e-5*tf.ones([6], dtype=tf.float32)), axis=0), [Q.shape[1], 1, 1])
+        norm2 = tf.tile(tf.expand_dims(norm1, axis=0), [Q.shape[0], 1, 1, 1])
+        inv_Q = tf.matrix_inverse(Q + norm2)
 
         # sum of determinants along the time
         sum_det_Q = tf.reduce_sum(log_det_Q, axis=0)