bf2bede944991679bd17d8ffc654fdeb5b09fc94
Chunshang Li <jack.chunshang.li@gmail.com>
2018-06-27 16:03:49 -0400

    added debugging code to fc loss scrap, reverted val sequence

--- train_seq.py
+++ train_seq.py
@@ -1,14 +1,14 @@
 from __future__ import absolute_import, division, print_function
 import config
+import train
 
 cfg = config.SeqTrainLidarConfig
-import train
 
 trainer = train.Train(num_gpu=2,
                       cfg=cfg,
                       train_sequences=["00", "01", "02", "08", "09"],
-                      #train_sequences=["04"],
-                      val_sequence="05",
+                      # train_sequences=["04"],
+                      val_sequence="07",
                       start_epoch=0,
                       restore_file=None)
 
