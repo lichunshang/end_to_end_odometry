360fdb5df06ade470656042b5b6a6659dc31103a
Chunshang Li <jack.chunshang.li@gmail.com>
2018-07-23 11:59:50 -0400

    state save file name change

--- train.py
+++ train.py
@@ -441,12 +441,12 @@ class Train(object):
                 best_val_loss = curr_val_loss
                 self.tf_saver_best.save(self.tf_session, self.best_val_path, global_step=i_epoch)
                 tools.printf("Best val loss, model saved.")
-                pickle.dump(ekf_states_dic, os.path.join(self.best_val_path, "best_val_ekf_states-%d" % i_epoch))
+                pickle.dump(ekf_states_dic, open(os.path.join(self.best_val_path, "best_val_ekf_states-%d.pickle" % i_epoch), "wb"))
             if i_epoch % 5 == 0:
                 tools.printf("Saving checkpoint...")
                 self.tf_saver_checkpoint.save(self.tf_session, self.model_epoch_path, global_step=i_epoch)
                 tools.printf("Checkpoint saved")
-                pickle.dump(ekf_states_dic, os.path.join(self.model_epoch_path, "model_epoch_ekf_states-%d" % i_epoch))
+                pickle.dump(ekf_states_dic, open(os.path.join(self.model_epoch_path, "model_epoch_ekf_states-%d.pickle" % i_epoch), "wb"))
 
             self.tf_tb_writer.flush()
             tools.printf("ave_val_loss(se3): %f, time: %f\n" % (curr_val_loss, time.time() - start_time))
@@ -458,6 +458,6 @@ class Train(object):
                                       os.path.join(self.results_dir_path, "model_epoch_checkpoint"),
                                       global_step=i_epoch)
         tools.printf("Saved results to %s" % self.results_dir_path)
-        pickle.dump(ekf_states_dic, os.path.join(self.model_epoch_path, "model_epoch_ekf_states-%d" % i_epoch))
+        pickle.dump(ekf_states_dic, open(os.path.join(self.model_epoch_path, "model_epoch_ekf_states-%d.pickle" % i_epoch), "wb"))
 
         self.tf_session.close()
