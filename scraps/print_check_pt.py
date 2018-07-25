ba92a299ffa04b1ddb63cdf8c46e3e9454f51736
Chunshang Li <jack.chunshang.li@gmail.com>
2018-07-25 12:32:07 -0400

    save ekf covars

--- scraps/print_check_pt.py
+++ scraps/print_check_pt.py
@@ -1,6 +1,6 @@
 from tensorflow.python.tools import inspect_checkpoint as chkp
 
-restore_model_file = "/media/cs4li/DATADisk/train_seq_20180528-10-34-14/best_val/model_best_val_checkpoint-195"
+restore_model_file = "/home/cs4li/Dev/end_to_end_odometry/results/train_seq_20180724-14-56-55/model_epoch_checkpoint-199"
 
 chkp.print_tensors_in_checkpoint_file(restore_model_file, tensor_name='',
-                                      all_tensors=False, all_tensor_names=True)
+                                      all_tensors=True, all_tensor_names=True)
--- train.py
+++ train.py
@@ -446,6 +446,8 @@ class Train(object):
                 tools.printf("Best val loss, model saved.")
                 pickle.dump(ekf_states_dic, open(os.path.join(self.best_val_path,
                                                               "best_val_ekf_states-%d.pickle" % i_epoch), "wb"))
+                pickle.dump(ekf_cov_states_dic, open(os.path.join(self.best_val_path,
+                                                                  "best_val_ekf_cov_states-%d.pickle" % i_epoch), "wb"))
             if i_epoch % 5 == 0:
                 tools.printf("Saving checkpoint...")
                 self.tf_saver_checkpoint.save(self.tf_session,
@@ -454,6 +456,8 @@ class Train(object):
                 tools.printf("Checkpoint saved")
                 pickle.dump(ekf_states_dic, open(os.path.join(self.model_epoch_path,
                                                               "model_epoch_ekf_states-%d.pickle" % i_epoch), "wb"))
+                pickle.dump(ekf_cov_states_dic, open(os.path.join(self.best_val_path,
+                                                                  "best_val_ekf_cov_states-%d.pickle" % i_epoch), "wb"))
 
             self.tf_tb_writer.flush()
             tools.printf("ave_val_loss(se3): %f, time: %f\n" % (curr_val_loss, time.time() - start_time))
@@ -466,5 +470,7 @@ class Train(object):
         tools.printf("Saved results to %s" % self.results_dir_path)
         pickle.dump(ekf_states_dic,
                     open(os.path.join(self.model_epoch_path, "model_epoch_ekf_states-%d.pickle" % i_epoch), "wb"))
+        pickle.dump(ekf_cov_states_dic,
+                    open(os.path.join(self.model_epoch_path, "model_epoch_ekf_cov_states-%d.pickle" % i_epoch), "wb"))
 
         self.tf_session.close()
