d74ccda1776374e39838b5a85106ebc864b84ada
Chunshang Li <jack.chunshang.li@gmail.com>
2018-08-02 15:49:56 -0400

    added script to plot errors and covars

---
+++ scraps/plot_errors_and_covars.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import config
+
+sequence = "06"
+data_dir = config.save_path + "/trajectory_results/"
+fc_errors_path = data_dir + "fc_covars_%s.npy" % sequence
+fc_covars_path = data_dir + "fc_errors_%s.npy" % sequence
+fc_errors = trajectory = np.load(fc_errors_path)
+fc_covars = trajectory = np.load(fc_covars_path)
+
+fc_errors_abs = np.abs(fc_errors)
+fc_covars_3sig = np.sqrt(fc_covars) * 3
+
+plot_names = ["X", "Y", "Z", "Roll", "Pitch", "Yaw"]
+
+for i in range(0, 6):
+    plt.figure(i)
+    plt.plot(fc_errors_abs[:, i], "r")
+    plt.plot(-fc_covars_3sig[:, i], "r")
+    plt.plot(fc_errors_abs[:, i], "b")
+    plt.plot(-fc_covars_3sig[:, i], "b")
+
+    plt.xlabel("Frame #")
+
+    plt.title("Sequence %s %s Error & Covars" % (sequence, plot_names[i]))
+
+    plt.savefig(data_dir + "fig_%s_%s_error_and_covars.png" % (sequence, plot_names[i]))
+
