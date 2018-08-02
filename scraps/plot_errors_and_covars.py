553124054cf6ad2cc5f35f88f4afd84cf524649e
Chunshang Li <jack.chunshang.li@gmail.com>
2018-08-02 16:02:10 -0400

    updateed plot errors covars

--- scraps/plot_errors_and_covars.py
+++ scraps/plot_errors_and_covars.py
@@ -4,8 +4,8 @@ import config
 
 sequence = "06"
 data_dir = config.save_path + "/trajectory_results/"
-fc_errors_path = data_dir + "fc_covars_%s.npy" % sequence
-fc_covars_path = data_dir + "fc_errors_%s.npy" % sequence
+fc_covars_path = data_dir + "fc_covars_%s.npy" % sequence
+fc_errors_path = data_dir + "fc_errors_%s.npy" % sequence
 fc_errors = trajectory = np.load(fc_errors_path)
 fc_covars = trajectory = np.load(fc_covars_path)
 
@@ -16,13 +16,14 @@ plot_names = ["X", "Y", "Z", "Roll", "Pitch", "Yaw"]
 
 for i in range(0, 6):
     plt.figure(i)
-    plt.plot(fc_errors_abs[:, i], "r")
-    plt.plot(-fc_covars_3sig[:, i], "r")
-    plt.plot(fc_errors_abs[:, i], "b")
-    plt.plot(-fc_covars_3sig[:, i], "b")
+    plt.plot(fc_errors_abs[1:, i], "r")
+    plt.plot(-fc_errors_abs[1:, i], "r")
+    plt.plot(fc_covars_3sig[1:, i], "b")
+    plt.plot(-fc_covars_3sig[1:, i], "b")
 
     plt.xlabel("Frame #")
 
+    plt.legend()
     plt.title("Sequence %s %s Error & Covars" % (sequence, plot_names[i]))
 
     plt.savefig(data_dir + "fig_%s_%s_error_and_covars.png" % (sequence, plot_names[i]))
