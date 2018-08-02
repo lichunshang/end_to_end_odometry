95e0085ad58d1928ca995c46346d557bb7a849cd
Chunshang Li <jack.chunshang.li@gmail.com>
2018-08-02 14:22:00 -0400

    save settings

--- config.py
+++ config.py
@@ -54,15 +54,15 @@ class SeqTrainLidarConfig(Configs):
     use_ekf = True
     train_noise_covariance = True  # Train the imu noise covariance for ekf
     static_nn = True  # don't modify the nn weights if set to true
-    fix_fc_covar = False
+    fix_fc_covar = True
     fc_covar_fix_val = np.array([0.1] * 6, dtype=np.float32)
     ekf_initial_state_covariance = 100  # initial covariance for all the states
-    init_gyro_bias_covar = 10
-    init_acc_bias_covar = 10
-    init_gyro_covar = 10
-    init_acc_covar = 10
+    init_gyro_bias_covar = 0.1
+    init_acc_bias_covar = 0.1
+    init_gyro_covar = 0.1
+    init_acc_covar = 0.1
 
-    train_ekf_with_fcgt = False  # train ekf using fc ground truth instead of nn outputs
+    train_ekf_with_fcgt = True  # train ekf using fc ground truth instead of nn outputs
     gt_init_vel_state = False  # use ground truth for initial velocity state for all batches at every epoch
     gt_init_vel_state_only_first = False  # if gt_init_vel_state=True, but only force the first epoch first batch
 
