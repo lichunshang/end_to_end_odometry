4385af8af4d654eeec951595bf83570a8bb1e9bf
Chunshang Li <jack.chunshang.li@gmail.com>
2018-08-07 15:39:11 -0400

    added ability to plot all seqs

--- scraps/plot_errors_and_covars.py
+++ scraps/plot_errors_and_covars.py
@@ -2,30 +2,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 import config
 
-sequence = "06"
-data_dir = config.save_path + "/trajectory_results/"
-fc_covars_path = data_dir + "fc_covars_%s.npy" % sequence
-fc_errors_path = data_dir + "fc_errors_%s.npy" % sequence
-fc_errors = trajectory = np.load(fc_errors_path)
-fc_covars = trajectory = np.load(fc_covars_path)
+sequences = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10"]
 
-fc_errors_abs = np.abs(fc_errors)
-fc_covars_3sig = np.sqrt(fc_covars) * 3
+for sequence in sequences:
 
-plot_names = ["X", "Y", "Z", "Yaw", "Pitch", "Roll"]
+    data_dir = config.save_path + "/trajectory_results/"
+    fc_covars_path = data_dir + "fc_covars_%s.npy" % sequence
+    fc_errors_path = data_dir + "fc_errors_%s.npy" % sequence
 
-for i in range(0, 6):
-    plt.figure(i)
-    plt.plot(fc_errors_abs[1:, i], "r")
-    plt.plot(-fc_errors_abs[1:, i], "r")
-    plt.plot(fc_covars_3sig[1:, i], "b")
-    plt.plot(-fc_covars_3sig[1:, i], "b")
+    try:
+        fc_errors = trajectory = np.load(fc_errors_path)
+        fc_covars = trajectory = np.load(fc_covars_path)
+    except FileNotFoundError:
+        print("Files for sequence %s does not exist" % sequence)
+        continue
 
-    plt.xlabel("Frame #")
+    fc_errors_abs = np.abs(fc_errors)
+    fc_covars_3sig = np.sqrt(fc_covars) * 3
 
-    # plt.legend()
-    plt.title("Sequence %s %s Error & Covars" % (sequence, plot_names[i]))
-    # plt.show()
+    plot_names = ["X", "Y", "Z", "Yaw", "Pitch", "Roll"]
 
-    plt.savefig(data_dir + "fig_%s_%s_error_and_covars.png" % (sequence, plot_names[i]))
+    for i in range(0, 6):
+        plt.figure(i)
+        plt.plot(fc_covars_3sig[1:, i], "b")
+        plt.plot(-fc_covars_3sig[1:, i], "b")
+        plt.plot(fc_errors_abs[1:, i] * 50, "r")
+        plt.plot(-fc_errors_abs[1:, i] * 50, "r")
 
+        plt.xlabel("Frame #")
+
+        # plt.legend()
+        plt.title("Sequence %s %s Error & Covars" % (sequence, plot_names[i]))
+        # plt.show()
+
+        plt.savefig(data_dir + "fig_%s_%02d_%s_error_and_covars.png" % (sequence, i, plot_names[i]))
+        plt.clf()
+
+    print("Sequence %s done." % sequence)
